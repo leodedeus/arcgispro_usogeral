aa71bfd6ae6c3a71fa7ede21131d1d8592e66cb1
Leonardo Rodrigues de Deus <117215392+leodedeus@users.noreply.github.com>
2023-02-03 13:38:39 -0300

    Update Atribuir_valor_null_para_campos.py

--- scripts_arcgispro/Atribuir_valor_null_para_campos.py
+++ scripts_arcgispro/Atribuir_valor_null_para_campos.py
@@ -10,18 +10,22 @@ camada = "np_upar"
 #criar uma lista dos campos da camada de interesse
 campos = arcpy.ListFields(camada)
 
+'''
 #criar um laço de repetição para ler cada campo do arquivo
 for campo in campos:
         #utiliza a ferramenta AssignDefaultToField com os argumentos: AssignDefaultToField(nome da camada, nome do campo, valor default)
         #como o valor default desejado é que o campo seja nulo, então basta escrever None, sem aspas, assim como é feito na calculadora de campo do arcgis
         arcpy.management.AssignDefaultToField(camada, campo.name, None)
+'''
 
 #se a camada for vetorial é preciso desconsiderar os campos object id e shape
 for campo in campos:
     if campo.type != 'OID' and campo.type != 'Geometry':
         arcpy.management.AssignDefaultToField(camada, campo.name, None)
-        
+
+'''        
 #criar um laço de repetição para ler cada campo do arquivo
 for campo in campos:
     #restringindo os campos de interesse aos que são do tipo texto
     if campo.type == 'String':
+'''
